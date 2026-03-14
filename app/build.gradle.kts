1591f608eeadf594e55bae4043a18a66eaaff618
Nguyen Quang Minh (NQM) <nguyenquangminh391@gmail.com>
2026-03-14 20:39:54 +0700

    build: 4.13.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "app.pwhs.blockads"
         minSdk = 24
         targetSdk = 36
-        versionCode = 28
-        versionName = "4.13.0"
+        versionCode = 29
+        versionName = "4.13.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
